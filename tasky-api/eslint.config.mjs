e4deaf6c83ce899230e8b11089e2ec363bde199c
brendanoc21 <brendanoconnor2030@gmail.com>
2024-12-22 19:29:47 +0000

    Added routing for Updating and Deleting a Task

--- tasky-api/eslint.config.mjs
+++ tasky-api/eslint.config.mjs
@@ -7,7 +7,7 @@ export default [
     files: ["**/*.js"],
     languageOptions: {
       globals: globals.node,
-      ecmaVersion: 2015, // ES6
+      ecmaVersion: 2018, // ES6
       sourceType: "module", // Use module format
     },
     rules: {
