330901659d28833fd626b50b6ff19dd9993302bf
brendanoc21 <brendanoconnor2030@gmail.com>
2024-12-22 21:34:18 +0000

    Mongo lab Integrated Tasky React App

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import tasksRouter from './api/tasks';
 import './db';
 import usersRouter from './api/users';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -27,6 +28,8 @@ app.use('/api/tasks', tasksRouter);
 app.use(errHandler);
 
 app.use('/api/users', usersRouter);
+
+app.use(cors());
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
