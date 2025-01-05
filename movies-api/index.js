37d697704f890429ec041be64823d9abcf48ba45
brendanoc21 <brendanoconnor2030@gmail.com>
2025-01-05 14:19:40 +0000

    Added Auth Middleware

--- movies-api/index.js
+++ movies-api/index.js
@@ -5,6 +5,7 @@ import usersRouter from './api/users';
 import './db';
 import defaultErrHandler from './errHandler'
 import moviesRouter from './api/movies';
+import authenticate from './authenticate';
 
 dotenv.config();
 
@@ -15,7 +16,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/users', usersRouter);
 app.use(defaultErrHandler);
-app.use('/api/movies', moviesRouter);
+app.use('/api/movies', authenticate, moviesRouter);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
